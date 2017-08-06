4d7d01bec77f6e15c72e71158385251f3431b427
Rachel Conner <rconner@hondros.com>
2017-08-05 21:49:33 -0400

    variant logo. messing with document idea

--- public/app/js/controllers/loginCtrl.js
+++ public/app/js/controllers/loginCtrl.js
@@ -4,7 +4,30 @@
         .controller('loginCtrl', loginCtrl);
     loginCtrl.$inject = ['$scope', 'authSrv'];
     function loginCtrl($scope, authSrv){
-        $scope.user = {};
+        $scope.user = {
+            isAuthenticated: function(){
+                return false;
+            }
+        };
+        $scope.filterDocs = {};
+        $scope.documents = [
+            {
+                name: 'June Minutes',
+                type: 'minutes'
+            },
+            {
+                name: 'July Minutes',
+                type: 'minutes'
+            },
+            {
+                name: 'Candlewood Lake Property Handbook 2017',
+                type: 'handbook'
+            },
+            {
+                name: 'Parade Permit',
+                type: 'misc'
+            }
+        ];
         $scope.login = function(){
             authSrv.login($scope.user);
         };
